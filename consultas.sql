db826d7dbf6bc4fb2944b4a97977106bd455b834
jenniferfreire <jennifer.freire@outlook.com>
2025-03-19 20:48:29 -0300

    perf: Adicionando consulta

--- consultas.sql
+++ consultas.sql
@@ -5,9 +5,12 @@ SELECT
     LOCAIS.endereco,
     LOCAIS.categoria,
     ACESSIBILIDADE.descricao AS tipo_acessibilidade
-FROM LOCAIS
-JOIN LOCAIS_ACESSIBILIDADES ON LOCAIS.id_local = LOCAIS_ACESSIBILIDADES.id_local
-JOIN ACESSIBILIDADE ON LOCAIS_ACESSIBILIDADES.id_acessibilidade = ACESSIBILIDADE.id_acessibilidade;
+FROM 
+    LOCAIS
+JOIN 
+    LOCAIS_ACESSIBILIDADES ON LOCAIS.id_local = LOCAIS_ACESSIBILIDADES.id_local
+JOIN 
+    ACESSIBILIDADE ON LOCAIS_ACESSIBILIDADES.id_acessibilidade = ACESSIBILIDADE.id_acessibilidade;
 
 
 -- Buscar locais que tenham um recurso específico
@@ -16,13 +19,30 @@ SELECT
     endereco,
     categoria,
     tipo_acessibilidade
-FROM LOCAIS_ACESSIBILIDADE_COMPLETA
-WHERE tipo_acessibilidade ILIKE '%libras%'; -- Substituir pelo termo desejado
+FROM 
+    LOCAIS_ACESSIBILIDADE_COMPLETA
+WHERE 
+    tipo_acessibilidade ILIKE '%libras%'; -- Substituir pelo termo desejado
 -- Caso queira realizar a busca por mais de um termo ao mesmo tempo:
 --   OR tipo_acessibilidade ILIKE '%elevador%';
 
 
 -- Contar quantos locais têm determinado recurso de acessibilidade
-SELECT COUNT(DISTINCT id_local) AS "Qtde. Locais Acessívies"
-FROM LOCAIS_ACESSIBILIDADE_COMPLETA
-WHERE tipo_acessibilidade ILIKE '%libras%'; -- Substituir pelo termo desejado
+SELECT 
+    COUNT(DISTINCT id_local) AS "Qtde. Locais Acessívies"
+FROM 
+    LOCAIS_ACESSIBILIDADE_COMPLETA
+WHERE 
+    tipo_acessibilidade ILIKE '%libras%'; -- Substituir pelo termo desejado
+
+
+-- Listar todas as categorias de locais e a quantidade de locais em cada uma.
+SELECT 
+	categoria,
+	COUNT(DISTINCT id_local) AS "Qtde. Locais"
+FROM 
+    locais_acessibilidade_completa
+GROUP BY 
+    categoria
+ORDER BY 
+    "Qtde. Locais" DESC;
